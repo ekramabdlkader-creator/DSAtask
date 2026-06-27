332ad2c5c7d2c94f71d26415ba404f25c4a79d1a
ekramabdlkader-creator <ekramabdlkader@gmail.com>
2026-06-27 09:45:46 -0700

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,9 @@
+x=int(input("enter the first numeber "))
+y=int(input("enter the second  number"))
+
+print("x+y =",x+y)
+print("x-y=",x-y)
+print("x*y=",x*y)
+print("x/y=",x/y)
+print("x%y=",x%y)
+print("finished")
